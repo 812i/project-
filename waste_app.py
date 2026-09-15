bd9e965bb7ba3020093bdb17516ec81b2cced708
812i <nora44work@gmail.com>
2026-09-15 20:39:46 +0300

    Create waste_app.py

---
+++ waste_app.py
@@ -0,0 +1,17 @@
+ai-waste-sorter/
+│
+├── dataset/
+│   ├── train/
+│   │   ├── metal/
+│   │   ├── organic/
+│   │   ├── paper/
+│   │   └── plastic/
+│   └── validation/
+│       ├── metal/
+│       ├── organic/
+│       ├── paper/
+│       └── plastic/
+├── models/
+├── train.py
+├── waste_app.py
+└── requirements.txt
